f754375d35fd63091a86dedd8cfb5f7c5e5d139c
Brian Bolt <brian.bolt@boltengineered.com>
2019-08-17 10:43:59 -0700

    bumping docker-compose version, api view installation script wait for db to be up

--- templates/copy_api_views_to_db_container.sh
+++ templates/copy_api_views_to_db_container.sh
@@ -6,5 +6,9 @@ folder=$(basename $(pwd));
 project=${COMPOSE_PROJECT_NAME:-$folder};
 parsed=$(echo "${project//[^A-Za-z0-9 ]/}" | tr [:upper:] [:lower:]);
 container=$parsed'_db_1';
+while [ "`docker inspect -f {% raw %}'{{.State.Running}}' {% endraw %} $container`" != "true" ]; do 
+   echo "waiting on the db to start";
+   sleep 2;
+done;
 docker cp api_views.sql $container:/tmp/api_views.sql
 /usr/local/bin/docker-compose exec db psql -U postgres acas -f "/tmp/api_views.sql"
